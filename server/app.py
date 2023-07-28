dad753781a510e0d9bfb1ea9d3fb6dd16a8f9b32
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-07-29 00:25:27 +0300

    Restaurant and Customer Login and Signup

--- server/app.py
+++ server/app.py
@@ -1,12 +1,178 @@
-from flask import Flask
+from flask import Flask, request, make_response, jsonify, session
 from flask_migrate import Migrate
-from models import db
+from flask_restful import Api, Resource
+from models import db, Customer, Admin, Restaurant, MenuItem, Order, OrderItem, PaymentTransaction, Review, LoyaltyProgram, Promotion, StaffMapping
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///foodchapchap.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
+
+api = Api(app)
+
+# INDEX ROUTE
+# @app.route('/')
+# def index():
+#     return "Index For FOODCHAPCHAP"
+
+class Index(Resource):
+    def get (self):
+        response_dict = {
+             "index" : "Welcome to FOODCHAPCHAP, a food pre-order service to satisfy ALL your cravings!",
+            }
+
+        response = make_response(jsonify(response_dict), 200,)
+        return response
+
+api.add_resource(Index, '/')
+
+
+# CUSTOMERS
+# Signup
+class CustomerSignup(Resource):
+    def post(self):
+        customer_name = request.form['customer_name']
+        customer_email = request.form['customer_email']
+        customer_password = request.form['customer_password']
+        phone_number = request.form['phone_number']
+        address = request.form['address']
+         
+        # Check if all the required fields are being entered
+        if not customer_name or not customer_email or not customer_password:
+            response = make_response(jsonify({"error": "Please provide all required fields"}), 400)
+            return response
+
+        # Check if the customer with the same email already exists
+        existing_customer = Customer.query.filter_by(customer_email=customer_email).first()
+        if existing_customer:
+            response = make_response(jsonify({"error": "Customer with the same email already exists"}), 409)
+            return response
+        
+        new_customer = Customer(
+            customer_name=customer_name,
+            customer_email=customer_email,
+            customer_password=customer_password,
+            phone_number=phone_number,
+            address=address,
+        )
+
+        db.session.add(new_customer) 
+        db.session.commit()
+
+        response_dict = new_customer.to_dict()
+
+        response = make_response(jsonify(response_dict), 201)
+        return response
+
+api.add_resource(CustomerSignup, '/customersignup')
+
+# Login
+class CustomerLogin(Resource):
+    def post(self): 
+        customer_email = request.form['customer_email']
+        customer_password = request.form['customer_password']
+
+        # Check if all the required fields are being entered
+        if not customer_email or not customer_password:
+            response = make_response(jsonify({"error": "Please provide all required fields"}), 400)
+            return response
+        
+        customer = Customer.query.filter_by(customer_email=customer_email).first()
+
+        # Check if the correct fields are being entered
+        if not customer:
+            response = make_response(jsonify({'error': 'Invalid email or password.'}), 401)
+            return response
+        
+        if customer.customer_password != customer_password:
+            response = make_response(jsonify({'error': 'Invalid email or password.'}), 401)
+            return response
+        
+        response = make_response(jsonify({'message': 'Login successful.'}), 200)
+        return response
+    
+api.add_resource(CustomerLogin, '/customerlogin')
+
+
+#RESTAURANT
+# Signup
+
+class RestaurantSignup(Resource):
+    def post(self):
+        restaurant_name = request.form['restaurant_name']
+        restaurant_email = request.form['restaurant_email']
+        restaurant_password = request.form['restaurant_password']
+        location = request.form['location']
+        ambience = request.form['ambience']
+        cuisines_offered = request.form['cuisines_offered']
+        operating_hours = request.form['operating_hours']
+        contact_info = request.form['contact_info']
+
+        # Check if all the required fields are being entered
+        if not restaurant_name or not restaurant_email or not restaurant_password:
+            response = make_response(jsonify({"error": "Please provide all required fields"}), 400)
+            return response
+        
+        # Check if the customer with the same email already exists
+        existing_restaurant = Restaurant.query.filter_by(restaurant_email=restaurant_email).first()
+
+        if existing_restaurant:
+            response = make_response(jsonify({"error": "Restaurant with the same email already exists"}), 409)
+            return response
+        
+        new_restaurant = Restaurant(
+            restaurant_name = restaurant_name,
+            restaurant_email = restaurant_email,
+            restaurant_password = restaurant_password,
+            location = location,
+            ambience= ambience,
+            cuisines_offered = cuisines_offered,
+            operating_hours = operating_hours,
+            contact_info = contact_info,
+        )
+
+        db.session.add(new_restaurant) 
+        db.session.commit()
+
+        response_dict = new_restaurant.to_dict()
+
+        response = make_response(jsonify(response_dict), 201)
+        return response
+        
+api.add_resource(RestaurantSignup, '/restaurantsignup')
+
+# Login
+class RestaurantLogin(Resource):
+    def post(self): 
+        restaurant_email = request.form['restaurant_email']
+        restaurant_password = request.form['restaurant_password']
+
+        # Check if all the required fields are being entered
+        if not restaurant_email or not restaurant_password:
+            response = make_response(jsonify({"error": "Please provide all required fields"}), 400)
+            return response
+        
+        restaurant = Restaurant.query.filter_by(restaurant_email=restaurant_email).first()
+
+        # Check if the correct fields are being entered
+        if not restaurant:
+            response = make_response(jsonify({'error': 'Invalid email or password.'}), 401)
+            return response
+        
+        if restaurant.restaurant_password != restaurant_password:
+            response = make_response(jsonify({'error': 'Invalid email or password.'}), 401)
+            return response
+        
+        response = make_response(jsonify({'message': 'Login successful.'}), 200)
+        return response
+    
+api.add_resource(RestaurantLogin, '/restaurantlogin')
+
+
+
+if __name__ == '__main__':
+    app.run(port=5555)
 
--- server/models.py
+++ server/models.py
@@ -13,48 +13,28 @@ class Admin(db.Model, SerializerMixin):
     admin_username = db.Column(db.String(100), nullable=False, unique=True)
     admin_email = db.Column(db.String(120), nullable=False, unique=True)
     admin_password = db.Column(db.String(255), nullable=False)
-    customers = db.relationship('Customer', back_populates='admin')
-    restaurants = db.relationship('Restaurant', back_populates='admin')
-
-    serialize_rules = ('-customers.admin', '-restaurants.admin',)
+    
 
     def __repr__(self):
         return f'Admin(admin_id={self.admin_id}, admin_email={self.admin_email})'
 
-
-# ASSOCIATION TABLE CUSTOMER TO REVIEW
-customer_review_association = db.Table(
-    'customer_reviews',
-    db.Column('customer_id', db.Integer, db.ForeignKey('customers.customer_id'), primary_key=True),
-    db.Column('review_id', db.Integer, db.ForeignKey('reviews.review_id'), primary_key=True)
-)
 
 # CUSTOMER
 class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
     customer_id = db.Column(db.Integer, primary_key=True)
-    admin_id = db.Column(db.Integer, db.ForeignKey('admins.admin_id'), nullable=False)
-    customer_username = db.Column(db.String(100), nullable=False, unique=True)
+    customer_name = db.Column(db.String(100), nullable=False, unique=True)
     customer_email = db.Column(db.String(120), nullable=False, unique=True)
     customer_password = db.Column(db.String(255), nullable=False)
     phone_number = db.Column(db.String(20))
     address = db.Column(db.String(200))
-    orders = db.relationship('Order', back_populates = 'customer')
-    loyalty_programs = db.Relationship('LoyaltyProgram', back_populates = 'customer')
-
-    serialize_rules = ('-admin.customers', '-orders.customer', '-loyalty_programs.customer',)
 
     def __repr__(self):
         return f'Customer(customer_id={self.customer_id}, customer_email={self.customer_email})'
     
     
-# ASSOCIATION TABLE RESTAURANT TO PROMOTION
-restaurant_promotion_association = db.Table(
-    'restaurant_promotions',
-    db.Column('restaurant_id', db.Integer, db.ForeignKey('restaurants.restaurant_id'), primary_key=True),
-    db.Column('promotion_id', db.Integer, db.ForeignKey('promotions.promotion_id'), primary_key=True)
-)
+
 
 # RESTAURANT
 
@@ -62,7 +42,6 @@ class Restaurant(db.Model, SerializerMixin):
     __tablename__ = 'restaurants'
 
     restaurant_id = db.Column(db.Integer, primary_key=True)
-    admin_id = db.Column(db.Integer, db.ForeignKey('admins.admin_id'), nullable=False)
     restaurant_name = db.Column(db.String(200), nullable=False)
     restaurant_email = db.Column(db.String(120), nullable=False, unique=True)
     restaurant_password = db.Column(db.String(255), nullable=False)
@@ -71,14 +50,7 @@ class Restaurant(db.Model, SerializerMixin):
     cuisines_offered = db.Column(db.String(200), nullable=False)
     operating_hours = db.Column(db.String(200), nullable=False)
     contact_info = db.Column(db.String(200), nullable=False)
-    orders = db.relationship('Order', back_populates = 'restaurant')
-    reviews = db.Relationship('Review', back_populates = 'restaurant')
-    menuitems = db.Relationship('MenuItem', back_populates = 'restaurant')
-    staff_mappings = db.Relationship('StaffMapping', back_populates = 'restaurant') 
-    promotions = db.relationship('Promotion', secondary=restaurant_promotion_association, back_populates='restaurants')
     
-    serialize_rules = ('-admin.restaurants', '-menuitems.restaurant', '-staff_mappings.restaurant', '-reviews.restaurant', '-orders.restaurant',)
-
     def __repr__(self):
         return f'Restaurant(restaurant_id={self.restaurant_id}, restaurant_name={self.restaurant_name})'
     
@@ -88,13 +60,10 @@ class MenuItem(db.Model, SerializerMixin):
     __tablename__ = 'menuitems'
 
     item_id = db.Column(db.Integer, primary_key=True)
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.restaurant_id'), nullable = False)
     item_name = db.Column(db.String(200), nullable=False)
     item_category = db.Column(db.String(100), nullable=False)
     item_description = db.Column(db.String(500))
     price = db.Column(db.Float, nullable=False)
-
-    serialize_rules = ('-restaurant.menuitems',)
 
     def __repr__(self):
         return f'MenuItem(item_id={self.item_id}, item_name={self.item_name})'
@@ -105,15 +74,11 @@ class Order(db.Model, SerializerMixin):
     __tablename__ = 'orders'
 
     order_id = db.Column(db.Integer, primary_key=True)
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.restaurant_id'), nullable = False)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customers.customer_id'), nullable = False)
     order_status = db.Column(db.String(100), nullable=False)
     order_total = db.Column(db.Float, nullable=False)
     order_date = db.Column(db.DateTime, nullable=False)
-    order_items = db.Relationship('OrderItem', back_populates ='order')
-    payment_transaction = db.relationship('PaymentTransaction', back_populates='order')
 
-    serialize_rules = ('-restaurant.orders', '-customer.orders', '-order_items.order',)
+
 
     def __repr__(self):
         return f'Order(order_id={self.order_id}, order_status={self.order_status})'
@@ -124,11 +89,9 @@ class OrderItem(db.Model, SerializerMixin):
     __tablename__ = 'orderitems'
 
     order_item_id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, db.ForeignKey('orders.order_id'), nullable = False)
     quantity = db.Column(db.Integer, nullable=False)
     subtotal = db.Column(db.Float, nullable=False)
 
-    serialize_rules =('-order.order_items',)
 
     def __repr__(self):
         return f'OrderItem(order_item_id={self.order_item_id}, subtotal={self.subtotal})'
@@ -152,13 +115,8 @@ class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
 
     review_id = db.Column(db.Integer, primary_key=True)
-    admin_id = db.Column(db.Integer, db.ForeignKey('admins.admin_id'))
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.restaurant_id'), nullable=False)
     review_text = db.Column(db.Text, nullable=False)
     review_rating = db.Column(db.Integer, nullable=False)
-    reviews = db.relationship('Review', secondary=customer_review_association, back_populates='customers')
-
-    serialize_rules =('-restaurant.reviews',)
 
     def __repr__(self):
         return f'Review(review_id={self.review_id}, review_text={self.review_text})'
@@ -169,11 +127,8 @@ class LoyaltyProgram(db.Model, SerializerMixin):
     __tablename__ = 'loyaltyprograms'
 
     loyalty_program_id = db.Column(db.Integer, primary_key=True)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customers.customer_id'), nullable=False)
     loyalty_points = db.Column(db.Integer, nullable=False)
     loyalty_tier = db.Column(db.String(100))
-
-    serialize_rules =('-customer.loyalty_programs',)
 
     def __repr__(self):
         return f'LoyaltyProgram(loyalty_program_id={self.loyalty_program_id}, loyalty_points={self.loyalty_points})'
@@ -199,16 +154,11 @@ class StaffMapping(db.Model, SerializerMixin):
     __tablename__ = 'mappings'
 
     staff_id = db.Column(db.Integer, primary_key=True)
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.restaurant_id'), nullable = False)
     staff_name = db.Column(db.String(100), nullable=False)
     staff_role = db.Column(db.String(100), nullable=False)
 
-    serialize_rules =('-restaurant.staff_mapping',)
 
     def __repr__(self):
         return f'StaffMapping(staff_id={self.staff_id}, staff_name={self.staff_name})'
     
 
-
-
-    
