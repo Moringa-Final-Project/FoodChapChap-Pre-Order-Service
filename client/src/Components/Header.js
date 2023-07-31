9e701abc123c03eab3cabb0862e5c4bd89957e31
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2023-08-01 02:56:37 +0300

    Logo added, working on header

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import './Header.css'
+import logo from '../images/logo.png';
 
 const Header = () => {
   return (
-    <div>
-        Header
+    <div className='max-width header'>
+        <img 
+        src={logo}
+        alt='Food ChapChap Logo'
+        className='header-logo'
+        />
+    <div className='header-right'>
+        <div className='header-location-search-container'>
+            <div className='location-wrapper'>
+                <div className='location-icon-name'>
+                <i className="fi fi-rr-marker absolute-center location-icon"></i>
+                <div>Nairobi</div>
+                </div>
+                <i className="fi fi-rr-caret-down absolute-center"></i>
+            </div>
+        </div>
+    </div>
     </div>
   )
 }
