6654020ae1ff06cafb3752b8e76e3fee39d2de79
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2023-08-02 21:01:39 +0300

    Signup and Login updates

--- client/src/Login.js
+++ client/src/Login.js
@@ -2,21 +2,65 @@ import { useState } from 'react';
 import './Login.css';
 
 const Login = () => {
-    const [interest, setInterest]=useState('Choose a User type')
+
+  const [role, setRole] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    // Create the login request body in the format expected by the backend
+    const formData = new FormData();
+    formData.append('email', email);
+    formData.append('password', password);
+
+    // Send the login request to the backend endpoint
+    fetch('http://127.0.0.1:5000/login', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Login failed');
+        }
+      })
+      .then((data) => {
+        // Handle successful login response here (e.g., set user state, store token, redirect, etc.)
+        console.log(data);
+      })
+      .catch((error) => {
+        // Handle login error here (e.g., show error message to the user)
+        console.error(error);
+      });
+  };
+
   return (
     <div className='login'>
         <h1>Login</h1>
-        <select className='menu' onChange={(e)=>setInterest(e.target.value)}> 
-                <option>Choose a User type</option>
-                <option>Customer</option>
-                <option>Restaurant</option>
-                <option>Admin</option>
-        </select>
-        <form>
-            <input type={'email'} placeholder={'Email'} />
-            <input type={'password'} placeholder={'Password'} />
-            <button type={'submit'}>Login</button>
-        </form>
+        <form onSubmit={handleLogin}>
+        <input
+          type={'text'}
+          placeholder={'User role: Customer or Restaurant'}
+          value={role}
+          onChange={(e) => setRole(e.target.value)}
+        />
+        <input
+          type={'email'}
+          placeholder={'Email'}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type={'password'}
+          placeholder={'Password'}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type={'submit'}>Login</button>
+      </form>
     </div>
   )
 }
