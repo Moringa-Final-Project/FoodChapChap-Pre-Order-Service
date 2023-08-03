5afebee741b2677c8b3df4520cf427d3263d9fc7
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2023-08-03 15:06:58 +0300

    Working on explore page cards

--- client/src/Components/ExploreCard.js
+++ client/src/Components/ExploreCard.js
@@ -2,8 +2,10 @@ import React from 'react'
 import './ExploreCard.css'
 
 const ExploreCard = ({ restaurant }) => {
-    const name= restaurant?.restaurant_name ?? "";
-    const coverImg= restaurant?.image_url
+    const name = restaurant?.restaurant_name ?? "";
+    const coverImg = restaurant?.image_url;
+    const rating = restaurant?.rating;
+    const offers = restaurant?.offer;
 
   return (
     <div>ExploreCard</div>
