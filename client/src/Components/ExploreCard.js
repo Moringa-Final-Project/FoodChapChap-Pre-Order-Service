0864a3fd44e1f4ae53189ff41bb60b5ce22d9e3e
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2023-08-08 11:59:54 +0300

    View order button added

--- client/src/Components/ExploreCard.js
+++ client/src/Components/ExploreCard.js
@@ -13,50 +13,45 @@ const ExploreCard = ({ restaurant }) => {
     const cuisines = restaurant?.cuisines_offered;
     const bottomContainers = restaurant?.bottomContainers;
 
-    const handleMenuModalClose = () => {
-        setIsMenuModalOpen(false); 
+    const handleMenuModalOpen = () => {
+        setIsMenuModalOpen(true);
       };
     
-      const handleCardClick = () => {
-        setIsMenuModalOpen(true); 
-      };
+      const handleMenuModalClose = () => {
+        setIsMenuModalOpen(false);
+      };    
 
   return (
-    <div className='explore-card cur-po' onClick={handleCardClick} >
-      <div className='explore-card-cover' >
-        <img src={coverImg} alt={name} className='explore-card-image' />
-        <div className='offers absolute-center'>{offers}</div>
-      </div>
-      <div className='res-row'>
-        <div className='res-name'>{name}</div>
-        <div className='res-rating absolute-center'>
-          {rating} <i className='fi fi-rr-star absolute-center'></i>
+    <div className='explore-card cur-po'>
+        <div className='explore-card-cover'>
+            <img src={coverImg} alt={name} className='explore-card-image' />
+            <div className='offers absolute-center'>{offers}</div>
         </div>
-      </div>
-      <div className='res-row'>
-        <div className='res-cuisine'>
-          <span className='res-cuisine-tag'>{cuisines}</span>
+        <div className='res-row'>
+            <div className='res-name'>{name}</div>
+            <div className='res-rating absolute-center'>
+                {rating} <i className='fi fi-rr-star absolute-center'></i>
+            </div>
         </div>
-      </div>
-      <div>
-        <div className='card-separator'></div>
-        <div className='explore-bottom'>
-          <img
-            src={bottomContainers[0]?.image?.url}
-            alt={bottomContainers?.text}
-            style={{ height: '18px' }}
-          />
-          <div className='res-bottom-text'>{bottomContainers[0]?.text}</div>
+            <div className='res-row'>
+                <div className='res-cuisine'><span className='res-cuisine-tag'>{cuisines}</span></div>
+            </div>
+        <div>
+            <div className='card-separator'></div>
+            <div className='explore-bottom'>
+                <img src={bottomContainers[0]?.image?.url} alt={bottomContainers?.text} style={{height: '18px'}} />
+                <div className='res-bottom-text'>{bottomContainers[0]?.text}</div>
+            </div>
         </div>
-      </div>
-      <MenuModal
-        restaurantName={name}
-        menuItems={restaurant.menu_items}
-        isOpen={isMenuModalOpen}
-        onClose={handleMenuModalClose}
-      />
+        <button className='view-menu-button cur-po' onClick={handleMenuModalOpen}>View Menu</button>
+        <MenuModal
+            restaurantName={name}
+            menuItems={restaurant.menu_items}
+            isOpen={isMenuModalOpen}
+            onClose={handleMenuModalClose}
+        />
     </div>
-  );
-};
+  )
+}
 
 export default ExploreCard
