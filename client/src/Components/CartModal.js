14f50947a1b37dc6e01e5c99d8b1bb73e0e4a87f
Tamunotonye Briggs <tamunotonye.briggs@student.moringaschool.com>
2023-08-10 01:01:48 +0300

    Proceed to checkout button works, service fee and subtotal added at the bottom of cart

--- client/src/Components/CartModal.js
+++ client/src/Components/CartModal.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import Modal from 'react-modal';
 import { useCart } from './CartContext'; // Update the path
-import './CartModal.css'
+import './CartModal.css';
 
 const CartModal = ({ isOpen, onClose }) => {
-  const { cartItems } = useCart();
+  const { cartItems, removeFromCart, increaseQuantity, decreaseQuantity, calculateTotalPrice, checkoutComplete, handleCheckout } = useCart(); 
 
-  const calculateTotalPrice = () => {
-    return cartItems.reduce((total, item) => total + item.price, 0);
+  const calculateServiceFee = () => {
+    const totalPrice = calculateTotalPrice();
+    if (totalPrice <= 499  && totalPrice > 0) {
+      return 30;
+    } else if (totalPrice <= 999 && totalPrice > 499) {
+      return 50;
+    } else if (totalPrice <= 1499 && totalPrice > 999) {
+      return 100;
+    } else if (totalPrice <= 1999 && totalPrice > 1499) {
+      return 150;
+    } else if (totalPrice > 1999) {
+      return 200;
+    } else {
+      return 0;
+    }
+    
   };
 
   return (
@@ -18,24 +32,63 @@ const CartModal = ({ isOpen, onClose }) => {
       overlayClassName="cart-modal-overlay"
     >
       <div className="cart-modal-content">
-        <h2>Your Cart</h2>
-        <ul>
-          {cartItems.map((item) => (
-            <li key={item.id}>
-              <div>
-                <span>{item.item_name}</span>
-                <span>${item.price.toFixed(2)}</span>
-              </div>
-            </li>
-          ))}
-        </ul>
-        <div className="cart-total">
-          <span>Total:</span>
-          <span>${calculateTotalPrice().toFixed(2)}</span>
-        </div>
-        <button className="proceed-button"> // onClick=Handle checkout
-          Proceed to Checkout
-        </button>
+        {checkoutComplete ? (
+          <>
+            <h2>Order Successfully Placed!</h2>
+            <ul>
+              {cartItems.map((item) => (
+                <li key={item.id}>
+                  <div className='receipt-content'>
+                    <span className='receipt-item-name'>{item.item_name}</span>
+                    <span className='receipt-item-price'>Ksh{(item.price * item.quantity).toFixed(2)}</span>
+                    <span className='receipt-item-quantity'>x{item.quantity}</span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <div className="cart-total">
+              <span className='receipt-total'>Total:</span>
+              <span className='receipt-total-price'>Ksh{(calculateTotalPrice() + calculateServiceFee()).toFixed(2)}</span>
+            </div>
+          </>
+        ) : (
+          <>
+            <h2>Your Cart</h2>
+            <ul>
+              {cartItems.map((item) => (
+                <li key={item.id}>
+                  <div className='cart-item-container'>
+                    <span className='item-name'>{item.item_name}</span>
+                    <div className='quantity-container'>
+                      <button className='quantity-button' onClick={() => decreaseQuantity(item.id)}>-</button>
+                      <span>{item.quantity}</span>
+                      <button className='quantity-button' onClick={() => increaseQuantity(item.id)}>+</button>
+                    </div>
+                    <span>Ksh{(item.price * item.quantity).toFixed(2)}</span>
+                    <button className='remove-button' onClick={() => removeFromCart(item.id)}>Remove</button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <div className="cart-service-fee">
+              <span className='total-text'>Service Fee:</span>
+              <span className='total-amount'>Ksh{calculateServiceFee().toFixed(2)}</span>
+            </div>
+            <div className="cart-subtotal">
+              <span className='total-text'>Subtotal:</span>
+              <span className='total-amount'>Ksh{calculateTotalPrice().toFixed(2)}</span>
+            </div>
+            <div className="cart-total">
+              <span className='total-text'>Total:</span>
+              <span className='total-amount'>Ksh{(calculateTotalPrice() + calculateServiceFee()).toFixed(2)}</span>
+            </div>
+            <div className='checkout-button-container'>
+              <button className="proceed-button" onClick={handleCheckout}>
+                Pay at Restaurant
+              </button>
+            </div>
+          </>
+        )}
         <button className="close-button" onClick={onClose}>
           Close
         </button>
--- client/src/Components/MenuModal.js
+++ client/src/Components/MenuModal.js
@@ -8,70 +8,70 @@ const MenuModal = ({ restaurantName, isOpen, onClose, onAddToCart }) => {
     {
       "id": 1,
       "image": "https://source.unsplash.com/random/?pizza&1",
-      "price": 9.99,
+      "price": 1480,
       "item_name": "Pizza",
       "desription": "Our Specially made Pizza"
     },
     {
       "id": 2,
       "image": "https://source.unsplash.com/random/?Burger&1",
-      "price": 6.49,
+      "price": 900,
       "item_name": "Burger",
       "desription": "Our Specially made Burger"
     },
     {
       "id": 3,
       "image": "https://source.unsplash.com/random/?Fries&1",
-      "price": 3.79,
+      "price": 200,
       "item_name": "Fries",
       "desription": "Our Specially made Fries"
     },
     {
       "id": 4,
       "image": "https://source.unsplash.com/random/?Sushi&1",
-      "price": 12.99,
+      "price": 1800,
       "item_name": "Sushi",
       "desription": "Our Specially made Sushi"
     },
     {
       "id": 5,
       "image": "https://source.unsplash.com/random/?Pasta&1",
-      "price": 8.99,
+      "price": 1500,
       "item_name": "Pasta",
       "desription": "Our Specially made Pasta"
     },
     {
       "id": 6,
       "image": "https://source.unsplash.com/random/?Salad&1",
-      "price": 5.49,
+      "price": 150,
       "item_name": "Salad",
       "desription": "Our Specially made Salad"
     },
     {
       "id": 7,
       "image": "https://source.unsplash.com/random/?Icecream&1",
-      "price": 7.99,
+      "price": 350,
       "item_name": "Ice Cream",
       "desription": "Our Specially made Ice Cream"
     },
     {
       "id": 8,
       "image": "https://source.unsplash.com/random/?Donut&1",
-      "price": 4.25,
+      "price": 300,
       "item_name": "Donut",
       "desription": "Our Specially made Donut"
     },
     {
       "id": 9,
       "image": "https://source.unsplash.com/random/?Cupcake",
-      "price": 2.99,
+      "price": 300,
       "item_name": "Cupcake",
       "desription": "Our Specially made Cupcake"
     },
     {
       "id": 10,
       "image": "https://source.unsplash.com/random/?Steak",
-      "price": 6.99,
+      "price": 850,
       "item_name": "Steak",
       "desription": "Our Specially made Steak"
     },
@@ -96,7 +96,7 @@ const MenuModal = ({ restaurantName, isOpen, onClose, onAddToCart }) => {
               <div className='first-container'>
                 <span className='item-name'>{item.item_name}</span>
                 <div className='item-description'>{item.desription}</div>
-                <span className='item-price'>${item.price.toFixed(2)}</span>
+                <span className='item-price'>Ksh{item.price.toFixed(2)}</span>
               </div>
               <div className='second-container'>
                 <div>
