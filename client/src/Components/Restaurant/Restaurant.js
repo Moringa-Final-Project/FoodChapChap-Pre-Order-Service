44c52f4a9ce1d2d7ffd8bd047a1b1129bcc5e13a
+ <+>
2023-08-10 22:38:11 +0300

    the filtering works

--- client/src/Components/Restaurant/Restaurant.js
+++ client/src/Components/Restaurant/Restaurant.js
@@ -1,13 +1,76 @@
+// import React, { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import Menu from './Restaurants/Menu';
+// import OrderList from './Restaurants/Order';
+// import './Restaurants.css';
+
+// function Restaurant() {
+//   const [menuVisible, setMenuVisible] = useState(false);
+
+//   // Sample order data (Replace this with your actual order data)
+//   const [orders] = useState([
+//     // ... (your order data here)
+//     { id: 1, item: 'Pizza', quantity: 1, imageUrl: "https://shorturl.at/qBLNY"},
+//     { id: 2, item: 'Burger', quantity: 1, imageUrl: "https://shorturl.at/cenxN" },
+//     { id: 3, item: 'Fries', quantity: 2, imageUrl: " https://shorturl.at/enwMY"},
+//     { id: 4, item: 'Sushi', quantity: 1, imageUrl: "https://shorturl.at/mnNQ3"},
+//     { id: 5, item: 'Pasta', quantity: 1,  imageUrl: "https://shorturl.at/sAS28"},
+//   ]);
+
+//   const [searchTerm, setSearchTerm] = useState('');
+//   const navigate = useNavigate();
+
+//   const handleSearch = (e) => {
+//     setSearchTerm(e.target.value);
+//   };
+
+//   const filteredOrders = orders.filter((order) =>
+//     order.item.toLowerCase().includes(searchTerm.toLowerCase())
+//   );
+
+//   return (
+//     <div className="App">
+//       <header className="head">
+//         <h1>FOOD CHAPCHAP</h1>
+//         <nav>
+//           <ul className="navbar">
+//             <li>
+//               <button onClick={() => setMenuVisible(false)}>Menu</button>
+//               <button onClick={() => setMenuVisible(true)}>OrderList</button>
+//             </li>
+//             <li>
+//               <input
+//                 type="text"
+//                 placeholder="Search"
+//                 value={searchTerm}
+//                 onChange={handleSearch}
+//                 className="search-bar"
+//               />
+//             </li>
+//           </ul>
+//           <div className='logout-button-container'>
+//             <button className='logout-button' onClick={() => navigate('/login')}>Logout</button>
+//           </div>
+//         </nav>
+//       </header>
+//       {menuVisible ? (
+//         <OrderList orders={filteredOrders} />
+//       ) : (
+//         <Menu />
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Restaurant;
+
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import Menu from './Restaurants/Menu';
 import OrderList from './Restaurants/Order';
 import './Restaurants.css';
 
 function Restaurant() {
   const [menuVisible, setMenuVisible] = useState(false);
-
-  // Sample order data (Replace this with your actual order data)
   const [orders] = useState([
     // ... (your order data here)
     { id: 1, item: 'Pizza', quantity: 1, imageUrl: "https://shorturl.at/qBLNY"},
@@ -16,9 +79,7 @@ function Restaurant() {
     { id: 4, item: 'Sushi', quantity: 1, imageUrl: "https://shorturl.at/mnNQ3"},
     { id: 5, item: 'Pasta', quantity: 1,  imageUrl: "https://shorturl.at/sAS28"},
   ]);
-
   const [searchTerm, setSearchTerm] = useState('');
-  const navigate = useNavigate();
 
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
@@ -34,6 +95,7 @@ function Restaurant() {
         <h1>FOOD CHAPCHAP</h1>
         <nav>
           <ul className="navbar">
+            {/* ... */}
             <li>
               <button onClick={() => setMenuVisible(false)}>Menu</button>
               <button onClick={() => setMenuVisible(true)}>OrderList</button>
@@ -48,15 +110,12 @@ function Restaurant() {
               />
             </li>
           </ul>
-          <div className='logout-button-container'>
-            <button className='logout-button' onClick={() => navigate('/login')}>Logout</button>
-          </div>
         </nav>
       </header>
       {menuVisible ? (
-        <OrderList orders={filteredOrders} />
+        <OrderList orders={filteredOrders} searchTerm={searchTerm} />
       ) : (
-        <Menu />
+        <Menu searchTerm={searchTerm} />
       )}
     </div>
   );
--- client/src/Components/Restaurant/Restaurants/Menu.js
+++ client/src/Components/Restaurant/Restaurants/Menu.js
@@ -1,7 +1,112 @@
-import React, { useState } from 'react';
-import './Menu.css'
+// import React, { useState } from 'react';
+// import './Menu.css'
 
-function Menu(){
+// function Menu(){
+//   const [menuItems, setMenuItems] = useState([
+//     { id: 101, item: 'Pizza', price: 1490, imageUrl: "https://shorturl.at/qBLNY" },
+//     { id: 102, item: 'Burger', price: 900, imageUrl: "https://shorturl.at/cenxN" },
+//     { id: 103, item: 'Fries', price: 200, imageUrl: " https://shorturl.at/enwMY" },
+//     { id: 104, item: 'Sushi', price: 1800, imageUrl: "https://shorturl.at/mnNQ3"},
+//     { id: 105, item: 'Pasta', price: 1500, imageUrl: "https://shorturl.at/sAS28"},
+//     {id: 106, item: 'Salad', price: 150, imageUrl: "https://shorturl.at/blAS6"},
+//     {id: 107, item: 'Ice Cream', price: 350, imageUrl: "https://shorturl.at/IKLW1"},
+//     {id: 108, item: 'Donut', price: 300, imageUrl: "https://shorturl.at/nPUYZ"},
+//     {id: 109, item: 'CupCake', price: 300, imageUrl: "https://shorturl.at/DHJO5"},
+//     {id: 110, item: 'Steak', price: 850, imageUrl: "https://shorturl.at/bpsvD"}
+//   ]);
+
+
+//   const [showUpdateForm, setShowUpdateForm] = useState(false);
+//   const [selectedItem, setSelectedItem] = useState(null);
+//   const [updatedItem, setUpdatedItem] = useState ({
+//     item: '',
+//     price: '',
+//   });
+
+//   const handleDelete = (itemId) => {
+//     setMenuItems((prevMenuItems) => prevMenuItems.filter((item) => item.id !== itemId));
+//   };
+
+//   const handleUpdate = (item) => {
+//     setSelectedItem(item)
+//     setUpdatedItem({ item: item.item, price: item.price });
+//     setShowUpdateForm(true);
+//   };
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setUpdatedItem((prevItem) => ({ ...prevItem, [name]: value }));
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     setMenuItems((prevMenuItems) => 
+//       prevMenuItems.map((item) => 
+//       item.id === selectedItem.id ? { ...item, item: updatedItem.item, price: updatedItem.price } : item
+//       )
+//     );
+//     setShowUpdateForm(false);
+//   }
+
+//   return (
+//     <div className="menu">
+//       <h2>Menu</h2>
+//       <div className="menu-cards">
+//         {menuItems.map((item) => (
+//           <div key={item.id} className="menu-card">
+//             <img src={item.imageUrl} alt={item.item} className="menu-item-image" />
+//             <h3>{item.item}</h3>
+//             <p>Price: Ksh{item.price}</p>
+//             <div className="menu-card-buttons">
+//               <button className="delete-button" onClick={() => handleDelete(item.id)}>
+//                 Delete
+//               </button>
+//               <button className="update-button" onClick={() => handleUpdate(item)}>
+//                 Update
+//               </button>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+
+//       {showUpdateForm && selectedItem && (
+//         <div className="update-form">
+//           <h3>Update Item</h3>
+//           <form onSubmit={handleSubmit}>
+//             <label>
+//               Name:
+//               <input
+//                 type="text"
+//                 name="item"
+//                 value={updatedItem.item}
+//                 onChange={handleChange}
+//               />
+//             </label>
+//             <label>
+//               Price:
+//               <input
+//                 type="number"
+//                 name="price"
+//                 value={updatedItem.price}
+//                 onChange={handleChange}
+//               />
+//             </label>
+//             <button type="submit">Update</button>
+//           </form>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Menu;
+
+
+import React, { useState } from 'react';
+import './Menu.css';
+
+function Menu({ searchTerm }) {
+  // ... (your existing code)
   const [menuItems, setMenuItems] = useState([
     { id: 101, item: 'Pizza', price: 1490, imageUrl: "https://shorturl.at/qBLNY" },
     { id: 102, item: 'Burger', price: 900, imageUrl: "https://shorturl.at/cenxN" },
@@ -13,6 +118,7 @@ function Menu(){
     {id: 108, item: 'Donut', price: 300, imageUrl: "https://shorturl.at/nPUYZ"},
     {id: 109, item: 'CupCake', price: 300, imageUrl: "https://shorturl.at/DHJO5"},
     {id: 110, item: 'Steak', price: 850, imageUrl: "https://shorturl.at/bpsvD"}
+    // Add more menu items here
   ]);
 
 
@@ -24,6 +130,7 @@ function Menu(){
   });
 
   const handleDelete = (itemId) => {
+    //Remove the item from the menuItems array by its id
     setMenuItems((prevMenuItems) => prevMenuItems.filter((item) => item.id !== itemId));
   };
 
@@ -40,6 +147,7 @@ function Menu(){
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    //Update the selected item with the new name and price
     setMenuItems((prevMenuItems) => 
       prevMenuItems.map((item) => 
       item.id === selectedItem.id ? { ...item, item: updatedItem.item, price: updatedItem.price } : item
@@ -48,27 +156,40 @@ function Menu(){
     setShowUpdateForm(false);
   }
 
+  const filteredMenuItems = menuItems.filter((item) =>
+    item.item.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="menu">
+      <div className='hidden'>
       <h2>Menu</h2>
+      <div className="filter-input">
+        <input
+          type="text"
+          placeholder="Search menu..."
+          value={searchTerm}
+          onChange={handleChange}
+        />
+      </div>
+      </div>
       <div className="menu-cards">
-        {menuItems.map((item) => (
-          <div key={item.id} className="menu-card">
-            <img src={item.imageUrl} alt={item.item} className="menu-item-image" />
-            <h3>{item.item}</h3>
-            <p>Price: Ksh{item.price}</p>
-            <div className="menu-card-buttons">
-              <button className="delete-button" onClick={() => handleDelete(item.id)}>
-                Delete
-              </button>
-              <button className="update-button" onClick={() => handleUpdate(item)}>
-                Update
-              </button>
-            </div>
-          </div>
+        {filteredMenuItems.map((item) => (
+           <div key={item.id} className="menu-card">
+           <img src={item.imageUrl} alt={item.item} className="menu-item-image" />
+           <h3>{item.item}</h3>
+           <p>Price: Ksh{item.price}</p>
+           <div className="menu-card-buttons">
+             <button className="delete-button" onClick={() => handleDelete(item.id)}>
+               Delete
+             </button>
+             <button className="update-button" onClick={() => handleUpdate(item)}>
+               Update
+             </button>
+           </div>
+         </div>
         ))}
       </div>
-
       {showUpdateForm && selectedItem && (
         <div className="update-form">
           <h3>Update Item</h3>
@@ -97,8 +218,6 @@ function Menu(){
       )}
     </div>
   );
-};
+}
 
 export default Menu;
-
-
